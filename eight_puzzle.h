8ce78d545f352106b365de98d60d529a1498f254
Aditya Verma <adityaverma@Adityas-MacBook-Pro.local>
2025-10-29 23:11:59 -0700

    Add eight_puzzle.cpp

--- eight_puzzle.h
+++ eight_puzzle.h
@@ -1,28 +1,39 @@
 #pragma once
+#include <array>
+#include <string>
+#include <vector>
+#include <set>
+#include <utility>
+#include <cstddef>
 
-class Node {
-    public:
-        Node* parent;
-        Node* left;
-        Node* right;
-        int value;
+using namespace std;
 
-        //ctor
-        Node() : parent(nullptr), left(nullptr), right(nullptr), value(0) {}
-        Node(int num) : parent(nullptr), left(nullptr), right(nullptr), value(num) {}
-        ~Node() {}
+struct Node {
+    array<int,9> state;
+    Node* parent;
+    string move;
+    int g;
+    int h;
+    int f;
+
+    Node(array<int,9> s, Node* p = nullptr, string mv = "", int g_n = 0, int h_n = 0)
+        : state(s), parent(p), move(mv), g(g_), h(h_), f(g_ + h_) {}
 };
 
-class Tree {
-    public:
-    Node* root;
+struct Problem {
+    array<int,9> initial{1,2,3,4,8,0,7,6,5};   // default puzzle
+    array<int,9> goal   {1,2,3,4,5,6,7,8,0};   // goal puzzle
 
-    //ctor
-    Tree() : root(nullptr) {}
-
-    // functions
-    void addNode(int);
-    void removeNode(int);
-    void drawPath();
-    int getNodeCount();
+    static bool isSolvable(const array<int,9>& s);
+    static vector<pair<array<int,9>,string>> neighbors(const array<int,9>& s);
 };
+
+struct SearchStats {
+    int nodesExpanded = 0;
+    size_t maxQueue = 0;
+};
+
+// Search prototypes
+Node* uniform_cost_search(const Problem&, SearchStats&, bool trace = true);
+Node* a_star_misplaced(const Problem&, SearchStats&, bool trace = true);
+Node* a_star_euclidean(const Problem&, SearchStats&, bool trace = true);
